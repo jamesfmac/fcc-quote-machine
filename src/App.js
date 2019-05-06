0cbc80ca07b98a5dd6efe923facf4d74837ef3b8
James McManamey <jfmcmanamey@gmail.com>
2019-05-05 22:03:50 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let style = {
+  width: '50%',
+  height: 200,
+  border: '4px solid black',
+  
+}
+
+class QuoteBox extends React.Component {
+
+  constructor(props){
+    super(props)
+
+  }
+
+  
+
+render(){
+  return(
+    <div id = 'quote-box' style = {style}> quotes go here</div>
+  )
+}
+
+}
+
+
+
+
+const appStyle = {
+ display: 'flex',
+ flexDirection: 'row',
+ height: '100vh',
+ justifyContent: 'center',
+ alignItems: 'center',
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style = {appStyle} >
+     <QuoteBox />
     </div>
   );
 }
