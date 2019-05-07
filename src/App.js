55e044674a69d8b42d2d1fc2b2a74e4fc8764e85
James McManamey <jfmcmanamey@gmail.com>
2019-05-07 14:01:40 -0700

    all test passing, no styling

--- src/App.js
+++ src/App.js
@@ -23,8 +23,10 @@ function Button (props){
 }
 
 function Tweet (props){
+  let text = encodeURI(props.quote)
+  let url = 'https://twitter.com/intent/tweet?text='+ text + ' - ' + encodeURI(props.author)
   return(
-    <a id ='tweet-quote' href = 'twitter.com/intent/tweet'> Tweet </a>
+    <a id ='tweet-quote' href = {url} target= '_blank' rel="noopener noreferrer"> Tweet </a>
   )
 }
 
@@ -39,17 +41,70 @@ class QuoteBox extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      quote: 'Initial quote', 
-      author: 'Initial author'
+      quote: '', 
+      author: '',
+      trumpIsNext: false
 
     }
   }
+  kanyeQuote = () => {
+    fetch('https://api.kanye.rest')
+  .then((response) => {
+    console.log(response)
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    this.setState({
+      quote: JSON.stringify(data.quote),
+      author: 'Kayne West',
+      trumpIsNext: true
+    })
+  })
+  .catch((error) => {
+    console.log(error, "catch the hoop")
+  });
+  }
+
+  trumpQuote = () => {
+    fetch('https://api.whatdoestrumpthink.com/api/v1/quotes/random')
+  .then((response) => {
+    console.log(response)
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    this.setState({
+      quote: JSON.stringify(data.message),
+      author: 'Donald Trump',
+      trumpIsNext: false
+    })
+  })
+  .catch((error) => {
+    console.log(error, "catch the hoop")
+  });
+  }
+
+  fetchQuote = () =>{
+
+    if(this.state.trumpIsNext){
+      this.trumpQuote()
+    }else{
+      this.kanyeQuote()
+    }
+
+  }
+
+
+
 
   handleClick(i){
-    this.setState({
-      quote: 'Second quote',
-      author: 'Second author'
-    })
+    this.fetchQuote();
+  }
+
+
+  componentDidMount(){
+    this.fetchQuote();
   }
 
 
@@ -58,10 +113,8 @@ render(){
     <div id = 'quote-box' style = {boxStyle}> 
     <Text quote = {this.state.quote} />
     <Author author = {this.state.author} />
-    <Tweet author = {this.state.author}/>
+    <Tweet quote = {this.state.quote} author = {this.state.author}/>
     <Button onClick = {(i) => this.handleClick(i)} />
-    
-    
     </div>
   )
 }
