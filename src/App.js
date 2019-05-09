db7de36628c0e3b1a568be569879b709f78e53a9
James McManamey <jfmcmanamey@gmail.com>
2019-05-08 23:32:42 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
 import React from 'react';
 import './App.css';
 
+let textStlye = {
+  padding: '20px 20px 5px 20px',
+  textAlign: 'justify',
+  fontSize: '1.75em',
+}
+
+let authorStyle = {
+  paddingRight: '20px',
+  alignSelf: 'flex-end',
+  fontSize: '1.0em',
+
+}
+
+let boxStyle = {
+  width: '50%',
+  minHeight: 200,
+  boxShadow: '0px 2px 4px rgba(0,0,0,0.18)',
+  borderRadius: '45px 0px',
+  backgroundImage: 'linear-gradient(to right top, #d3e1f5, #c5eaf8, #c0f2ef, #ccf6dd, #e9f6cb)',
+  display: 'flex',
+  flexDirection: 'column',
+  
+}
+
+let barStyle = {
+  marginTop: 'auto',
+    padding: '20px',
+    display:'flex',
+    flexDirection: 'row',
+    alignItems:'center',
+    justifyContent: 'space-between',
+
+}
+let buttonStyle = {
+  borderRadius:'4px',
+  height: 30,
+  fontSize: '1em',
+  background: 'ghostwhite',
+  boxShadow: '0px 2px 4px rgba(0,0,0,0.18)',
+
+}
+
+let tweetStyle = {
+  backgroundImage: 'Url(public/twitter-logo.png)'
+}
+
 
 
 
 function Text (props){
   return (
-    <div id = 'text'>{props.quote}</div>
+    <div id = 'text' style = {textStlye}>{props.quote}</div>
   )
 }
 
 function Author (props){
   return (
-    <div id = 'author'>{props.author}</div>
+    <div id = 'author' style = {authorStyle}> - {props.author}</div>
   )
 }
 
 function Button (props){
   return(
-    <button id ='new-quote' onClick = {props.onClick}> New Tweet</button>
+    <button id ='new-quote' onClick = {props.onClick}  style = {buttonStyle}> New Tweet</button>
   )
 }
 
@@ -26,17 +72,16 @@ function Tweet (props){
   let text = encodeURI(props.quote)
   let url = 'https://twitter.com/intent/tweet?text='+ text + ' - ' + encodeURI(props.author)
   return(
-    <a id ='tweet-quote' href = {url} target= '_blank' rel="noopener noreferrer"> Tweet </a>
+    <a id ='tweet-quote' href = {url} target= '_blank' rel="noopener noreferrer">
+    <img src ='/twitter-logo.png' width="40" height="40"/>   </a>
   )
 }
 
 
-let boxStyle = {
-  width: '50%',
-  height: 200,
-  border: '4px solid black',
-  
-}
+
+
+
+
 class QuoteBox extends React.Component {
   constructor(props){
     super(props)
@@ -108,14 +153,20 @@ class QuoteBox extends React.Component {
   }
 
 
+
 render(){
   return(
-    <div id = 'quote-box' style = {boxStyle}> 
-    <Text quote = {this.state.quote} />
-    <Author author = {this.state.author} />
-    <Tweet quote = {this.state.quote} author = {this.state.author}/>
-    <Button onClick = {(i) => this.handleClick(i)} />
-    </div>
+   
+      <div id = 'quote-box' style = {boxStyle}> 
+      <Text quote = {this.state.quote} />
+     
+      <Author author = {this.state.author} />
+      <div id = 'button-bar' style = {barStyle}>
+        <Tweet quote = {this.state.quote} author = {this.state.author}/>
+        <Button onClick = {(i) => this.handleClick(i)} />
+      </div>
+      </div>
+   
   )
 }
 
@@ -130,6 +181,7 @@ const appStyle = {
  height: '100vh',
  justifyContent: 'center',
  alignItems: 'center',
+
 }
 
 
